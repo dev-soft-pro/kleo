c67ed054ab5e183c2f56d5fb666599efd31bb8f7
CF <pclienth@hotmail.com>
2021-12-21 12:21:41 +0800

    limit width to 1440px

--- src/components/Top/index.js
+++ src/components/Top/index.js
@@ -4,29 +4,31 @@ import './styles.scss'
 export default function Top() {
   return (
     <div className="graphic-top" id="home">
-      <div className="top-content">
-        <div className="top-text">
-          <div className="title">What if a tutor and an app had a baby?</div>
-          <div className="content">
-            Kleo captures the magic of learning from a person - in your phone. 
-            A friend that guides you through a world where you can learn without limits and practice without judgement. 
-            Talk with Kleo anywhere, be understood everywhere</div>
-        </div>
-        <div className="apple-get">
-          <div className="title">Get the App</div>
-          <img src={AppleImage} alt="apple-link" />
-        </div>
-        <div className="signup-form">
-          <div className="title">
-            Sign up to get announcements: like new language or the Android release
+      <div className="graphic-wrapper">
+        <div className="top-content">
+          <div className="top-text">
+            <div className="title">What if a tutor and an app had a baby?</div>
+            <div className="content">
+              Kleo captures the magic of learning from a person - in your phone. 
+              A friend that guides you through a world where you can learn without limits and practice without judgement. 
+              Talk with Kleo anywhere, be understood everywhere</div>
           </div>
-          <div className="input">
-            <input type="text" />
-            <button type="button">Meet Kleo</button>
+          <div className="apple-get">
+            <div className="title">Get the App</div>
+            <img src={AppleImage} alt="apple-link" />
+          </div>
+          <div className="signup-form">
+            <div className="title">
+              Sign up to get announcements: like new language or the Android release
+            </div>
+            <div className="input">
+              <input type="text" />
+              <button type="button">Meet Kleo</button>
+            </div>
           </div>
         </div>
+        <img className="image-demo" src={Demo} alt="Demo" />
       </div>
-      <img className="image-demo" src={Demo} alt="Demo" />
     </div>
   )
 }
